2dc7405ca2d91120eb4c5b002cc42bbc712d9b11
Jason Chong <jaysonzai93@gmail.com>
2019-05-02 14:46:35 +0800

    added components in list

---
+++ src/List.js
@@ -0,0 +1,27 @@
+import React, { Component } from 'react'
+import { getList, addToList, deleteItem, updateItem } from '.ListFunctions'
+
+class List extends Component {
+    constructor() {
+        super()
+        this.state = {
+            id: '',
+            term: '',
+            editDisabled: false,
+            items: []
+        }
+
+        this.onSubmit = this.onSubmit.bind(this)
+        this.onChange = this.onChange.bind(this)
+    }
+
+    componentDidMount () {
+        this.getAll()
+    }
+
+    onChange = e => {
+        this.setState({
+            term: e.target.value,
+            editDisabled: 'disabled'
+        })
+    }
